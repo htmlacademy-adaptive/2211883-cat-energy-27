86deeb6d3ee24dd3595c79a9fcc56598a0d710af
Perry1510 <ruchkina1210@gmail.com>
2022-12-24 00:26:22 +0300

    добавлен вывод мобильного меню без js

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
 function openMenu(button) {
-  let menuWrapper = button.closest('.main-nav')?.querySelectorAll('.main-nav__wrapper');
+  let menuWrapper = document.querySelectorAll('.main-nav__wrapper');
   if (menuWrapper.length === 0) {
     return;
   }
   menuWrapper = menuWrapper[0];
 
   let menuOpenedClass = 'main-nav__wrapper--opened';
-  let menuButtonActiveClass = 'main-nav__toggle--active';
+  let menuButtonActiveClass = 'header__nav-toggle--active';
   if(menuWrapper.classList.contains(menuOpenedClass)) {
     menuWrapper.classList.remove(menuOpenedClass);
     button.classList.remove(menuButtonActiveClass);
@@ -15,3 +15,13 @@ function openMenu(button) {
     button.classList.add(menuButtonActiveClass);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let headerContainer = document.querySelectorAll('.header__container');
+  headerContainer = headerContainer.length === 0 ? false : headerContainer[0];
+  let headerContainerNoJsClass = 'header__container--no-js';
+
+  if(headerContainer && headerContainer.classList.contains(headerContainerNoJsClass)) {
+    headerContainer.classList.remove(headerContainerNoJsClass);
+  }
+});
